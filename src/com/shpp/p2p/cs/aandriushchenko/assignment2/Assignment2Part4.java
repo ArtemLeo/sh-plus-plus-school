025c020f00cf00b9de908fa202e86d0e70bc6907
Artem Andriushchenko <link.by.artem@gmail.com>
2023-09-18 06:20:51 +0300

    add Assignment2Part4

--- src/com/shpp/p2p/cs/aandriushchenko/assignment2/Assignment2Part4.java
+++ src/com/shpp/p2p/cs/aandriushchenko/assignment2/Assignment2Part4.java
@@ -5,6 +5,11 @@ import acm.program.*;
 
 import java.awt.*;
 
+/*
+  Problem # 4: Tricolor flag.
+  Write a program that draws a flag of three colored stripes of equal size.
+*/
+
 public class Assignment2Part4 extends GraphicsProgram {
 
     // Initialization of static constants.
@@ -15,6 +20,7 @@ public class Assignment2Part4 extends GraphicsProgram {
     private static final Color RED_COLOR = new Color(206, 17, 38);      // Red color.
     private static final Color BLACK_COLOR = new Color(9, 9, 9);        // Black color.
 
+    // Method run(): Get the size of the window, position the flag stripes, and add an inscription.
     public void run() {
         int windowWidth = getWidth();                            // Get window width.
         int windowHeight = getHeight();                          // Get window height.
@@ -32,33 +38,34 @@ public class Assignment2Part4 extends GraphicsProgram {
         whiteStripe.setColor(BLACK_COLOR);                       // Set the color of the border of the strip.
         whiteStripe.setFilled(true);                             // Fill the strip with color.
         whiteStripe.setFillColor(WHITE_COLOR);                   // Set the fill color of the strip.
-        add(whiteStripe, stripeWidth, 0);                    // Add and move strip to the width of the blue strip.
+        add(whiteStripe);                                        // Add a strip to the window.
 
         GRect redStripe = new GRect(stripeWidth, FLAG_HEIGHT);   // Create a strip of red color.
         redStripe.setColor(BLACK_COLOR);                         // Set the color of the border of the strip.
         redStripe.setFilled(true);                               // Fill the strip with color.
         redStripe.setFillColor(RED_COLOR);                       // Set the fill color of the strip.
-        add(redStripe, 2 * stripeWidth, 0);               // Add and move strip to the width of the blue and white strips.
+        add(redStripe);                                          // Add a strip to the window.
 
         // Find coordinates for centering the flag.
-        double x = (windowWidth - FLAG_WIDTH) / 2.0;             // Get coordinate X.
-        double y = (windowHeight - FLAG_HEIGHT) / 2.0;           // Get coordinate Y.
+        double x = (windowWidth - FLAG_WIDTH) / 2.0;       // Get coordinate X.
+        double y = (windowHeight - FLAG_HEIGHT) / 2.0;     // Get coordinate Y.
 
         // Position the stripes in the window.
-        blueStripe.setLocation(x, y);                            // Add a blueStripe to the window.
-        whiteStripe.setLocation(x + stripeWidth, y);          // Add and move strip to the width of the blueStripe.
-        redStripe.setLocation(x + 2 * stripeWidth, y);        // Add and move strip to the width of the blueStripe and whiteStripe.
+        blueStripe.setLocation(x, y);                      // Add a blueStripe to the window.
+        whiteStripe.setLocation(x + stripeWidth, y);    // Add and move strip to the width of the blueStripe.
+        redStripe.setLocation(x + 2 * stripeWidth, y);  // Add and move strip to the width of the blue and whiteStripe.
 
         // Add an inscription "Flag of France".
-        GLabel label = new GLabel("Flag of France");                  // Create an inscription.
-        label.setFont(new Font("SansSerif", Font.BOLD, 18));  // Set the font of the inscription.
-        double labelX = windowWidth - label.getWidth() - 10;              // Get the X to place in the right corner.
-        double labelY = windowHeight - 10;                                // Get the Y to place in the right corner.
-        add(label, labelX, labelY);                                       // Add an inscription to the window.
+        GLabel label = new GLabel("Flag of France");                   // Create an inscription.
+        label.setFont(new Font("SansSerif", Font.BOLD, 18));   // Set the font of the inscription.
+        double labelX = windowWidth - label.getWidth() - 10;               // Get the X to place in the right corner.
+        double labelY = windowHeight - 10;                                 // Get the Y to place in the right corner.
+        add(label, labelX, labelY);                                        // Add an inscription to the window.
     }
 
+    // Method main(): Running the application
     public static void main(String[] args) {
-        new Assignment2Part4().start();                          // Create an object of the class and run the program.
+        new Assignment2Part4().start();     // Create an object of the class and run the program.
     }
 }
 
