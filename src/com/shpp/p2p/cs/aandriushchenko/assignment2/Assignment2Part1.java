4e563830dade1b3269c75c74a93438c30b2eca90
Artem Andriushchenko <link.by.artem@gmail.com>
2023-09-19 06:43:55 +0300

    add Assignment2Part6

--- src/com/shpp/p2p/cs/aandriushchenko/assignment2/Assignment2Part1.java
+++ src/com/shpp/p2p/cs/aandriushchenko/assignment2/Assignment2Part1.java
@@ -29,30 +29,30 @@ public class Assignment2Part1 {
      */
     private static void enterTheValuesOfCoefficients() {
         Scanner scanner = new Scanner(System.in);             // Create a Scanner object.
-        System.out.println("Enter the coefficient a:");       // Ask the user to enter the coefficient "a"
+        System.out.println("Please enter a:");                // Ask the user to enter the coefficient "a"
         a = scanner.nextDouble();                             // Save the gotten value in the variable "a"
-        System.out.println("Enter the coefficient b:");       // Ask the user to enter the coefficient "b"
+        System.out.println("Please enter b:");                // Ask the user to enter the coefficient "b"
         b = scanner.nextDouble();                             // Save the gotten value in the variable "b"
-        System.out.println("Enter the coefficient c:");       // Ask the user to enter the coefficient "c"
+        System.out.println("Please enter c:");                // Ask the user to enter the coefficient "c"
         c = scanner.nextDouble();                             // Save the gotten value in the variable "c"
     }
 
     /*
      * Method: findTheRootsOfQuadraticEquation()
-     * Description: We calculate the discriminant and based on the result we find the root of the quadratic equation
+     * Description: Calculate the discriminant and based on the result we find the root of the quadratic equation
      */
     private static void findTheRootsOfQuadraticEquation() {
-        double discriminant = b * b - 4 * a * c;                      // Calculate the discriminant
-        if (a == 0) {                                                 // Check if the equation is linear
+        double discriminant = b * b - 4 * a * c;                        // Calculate the discriminant
+        if (a == 0) {                                                   // Check if the equation is linear
             System.out.println("This is not a quadratic equation");
-        } else if (discriminant > 0) {                                // Check if the discriminant is positive
-            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);  // Calculate the first root
-            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);  // Calculate the second root
+        } else if (discriminant > 0) {                                  // Check if the discriminant is positive
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);    // Calculate the first root
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);    // Calculate the second root
             System.out.println("There are two roots: " + root1 + " and " + root2);
-        } else if (discriminant == 0) {                               // Check if the discriminant is zero
-            double root = -b / (2 * a);                               // Calculate the root
+        } else if (discriminant == 0) {                                 // Check if the discriminant is zero
+            double root = -b / (2 * a);                                 // Calculate the root
             System.out.println("There is one root: " + root);
-        } else {                                                      // In other cases the discriminant is negative
+        } else {                                                        // In other cases the discriminant is negative
             System.out.println("There are no real roots!");
         }
     }
